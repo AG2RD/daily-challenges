9540cbbc4171f54c021d6ecf1baa6dfb865f0ccf
Adrien Gerard <agerard@altissia.org>
2020-10-11 22:20:54 +0200

    feat(pomodoro-vanilla): wip

--- challenge-october-01/src/js/pomodoro.js
+++ challenge-october-01/src/js/pomodoro.js
@@ -1,4 +1,40 @@
 "use strict";
-(function pomodoro() {
-  console.log('pomodoro');
-})();
+
+window.addEventListener('load', function () {
+  pomodoro();
+});
+
+function pomodoro() {
+  const workDurationInMin = 15;
+  const pauseDurationInMin = 5;
+  let intervalId;
+  let cpt = workDurationInMin * 60;
+  initCountdownView();
+
+  document.getElementById('play').addEventListener("click", () => start(cpt));
+
+  function initCountdownView() {
+    document.getElementById('min').innerHTML = workDurationInMin + "";
+    document.getElementById('sec').innerHTML = "00";
+  }
+
+  function start(currentCpt) {
+    console.log('pomodoro start');
+    intervalId = setInterval(() => {
+      cpt = --currentCpt;
+      document.getElementById('min').innerHTML = (currentCpt > 0 ? Math.floor(cpt / 60) : 15) + "";
+      const totSec = currentCpt % 60;
+      document.getElementById('sec').innerHTML = totSec < 10 ? "0" + totSec : totSec + "";
+    }, 1000);
+    return true;
+  }
+
+  function pause() {
+    window.clearInterval(intervalId);
+  }
+
+  function reset() {
+    initCountdownView();
+  }
+
+};
